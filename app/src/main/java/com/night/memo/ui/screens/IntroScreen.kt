e69b9ef91763fb7dad2e29a4bc78cf7583a565ba
root <root@localhost.localdomain>
2026-05-20 07:05:31 +0000

    Fix: remove interactionSource param from combinedClickable
    - combinedClickable in Foundation 1.6.x doesn't have interactionSource param
    - Remove MutableInteractionSource import and usage
    - Remove unused remember import

--- app/src/main/java/com/night/memo/ui/screens/IntroScreen.kt
+++ app/src/main/java/com/night/memo/ui/screens/IntroScreen.kt
@@ -3,7 +3,6 @@ package com.night.memo.ui.screens
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,7 +20,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -47,7 +45,6 @@ fun IntroScreen(
     modifier: Modifier = Modifier
 ) {
     val haptic = LocalHapticFeedback.current
-    val interactionSource = remember { MutableInteractionSource() }
 
     Column(
         modifier = modifier
@@ -91,7 +88,6 @@ fun IntroScreen(
                 .clip(RoundedCornerShape(16.dp))
                 .background(MaterialTheme.colorScheme.primary)
                 .combinedClickable(
-                    interactionSource = interactionSource,
                     onClick = onGetStarted,
                     onLongClick = {
                         haptic.performHapticFeedback(HapticFeedbackType.LongPress)
